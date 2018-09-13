97b4828b8acb27c4f4fc4c1dcfe85eed3eab0c45
Dylan Barva <22990096+DylanB5402@users.noreply.github.com>
2018-09-13 14:08:21 -0700

    Weather stuffs part 2

--- BlueJ/Weather/StringTester.java
+++ BlueJ/Weather/StringTester.java
@@ -11,8 +11,14 @@ public class StringTester {
     // instance variables - replace the example below with your own
     public static void main() {
         weatherCondition = "freezing drizzle";
-        System.out.println(weatherCondition.length());
-        System.out.println(weatherCondition.substring(0, weatherCondition.indexOf(" ")));
-        System.out.println(WeatherConditionals.getWeatherAdvice(32, "freezing drizzle"));
+        System.out.println(getFirstWord(weatherCondition));
+        System.out.println(WeatherConditionals.getWeatherAdvice(32, "snowy snow"));
+ 
+   
     }
+    
+    public static String getFirstWord(String word) {
+        return word.substring(0, word.indexOf(" "));
+    }
+    
 }
--- BlueJ/Weather/WeatherConditionals.java
+++ BlueJ/Weather/WeatherConditionals.java
@@ -5,15 +5,38 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class WeatherConditionals
-{
+public class WeatherConditionals {
+    
+    private static Boolean windy, snow;
+    private static String shortCondition;
+    
     public static String getWeatherAdvice(int temperature, String condition) {
-        Boolean windy;
         
-        if (condition.toLowerCase().contains("windy")) {
-            windy = true;
+        
+        if (temperature >= 100 && condition.toLowerCase().contains("snow")) {
+            return "I somehow doubt it's snowing in " + Integer.toString(temperature) + " degree heat";
+        }
+        else if (temperature <= 32) {
+            return "Too windy or cold! Enjoy watching the weather through the window.";
+        }
+        else if (temperature > 32) {
+            if (condition.toLowerCase().contains("windy")) {
+                windy = true;
+                shortCondition = "windy";
+            }
+            else if (condition.toLowerCase().contains("snow")) {
+                snow = true;
+                shortCondition = "snow";
+            }
+            else if (condition.toLowerCase().contains("sunny")) {
+                snow = true;
+                shortCondition = "sunny";
+            }
+            return "It's safe to go outside, " + Integer.toString(temperature) + " degrees and " + shortCondition + ".";
+        }
+        else {
+            return (Integer.toString(temperature) + " degrees and " + condition);
         }
         
-        return (Integer.toString(temperature) + " degrees and " + condition);
     }
 }
